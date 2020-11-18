04013ef7c4a4f4dfee4422d2b3fad3d64578370b
Robotix_00 <69535212+Robotix-00@users.noreply.github.com>
2020-11-18 09:44:55 +0100

    Eine neue Version
    Ich mag die Zahl 3 mehr als 2

--- src/de/serverone/source/util/Cooldown.java
+++ src/de/serverone/source/util/Cooldown.java
@@ -8,28 +8,28 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Cooldown {
-	private int cooldownTime;
-	private JavaPlugin plugin;
-	private List<String> list = new ArrayList<>();
-	
-	
-	public Cooldown(int cooldownTime, JavaPlugin plugin) {
-		this.cooldownTime = cooldownTime;
-		this.plugin = plugin;
-	}
-	
-	public boolean run(Player player) {
-		if(list.contains(player.getName())) return false;
-		else {
-			list.add(player.getName());
-			
-			Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-				@Override
-				public void run() {
-					list.remove(player.getName());
-				}
-			}, cooldownTime);
-			return true;
+    private int cooldownTime;
+    private JavaPlugin plugin;
+    private List<String> list = new ArrayList<>();
+
+    public Cooldown(int cooldownTime, JavaPlugin plugin) {
+	this.cooldownTime = cooldownTime;
+	this.plugin = plugin;
+    }
+
+    public boolean run(Player player) {
+	if (list.contains(player.getName()))
+	    return false;
+	else {
+	    list.add(player.getName());
+
+	    Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+		@Override
+		public void run() {
+		    list.remove(player.getName());
 		}
+	    }, cooldownTime);
+	    return true;
 	}
+    }
 }
